fb4fed7df115c805b5c0fab0808c1e353ddf92c0
ebenner91 <ebenner@mail.greenriver.edu>
2017-10-13 00:20:53 -0700

    Got play again button working

--- app/src/main/java/edu/greenriver/it/guessinggame/ResultsActivity.java
+++ app/src/main/java/edu/greenriver/it/guessinggame/ResultsActivity.java
@@ -1,12 +1,16 @@
 package edu.greenriver.it.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
     TextView report;
+    Button replay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,22 +18,35 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         getTextView();
-        receiveIntent();
+        getButton();
+        reportCount();
+        replayListener();
     }
 
     void getTextView() {
         report = (TextView) findViewById(R.id.countReport);
     }
+    void getButton() {
+        replay = (Button) findViewById(R.id.replayButton);
+    }
 
-    void receiveIntent() {
+    void reportCount() {
         if(getIntent().hasExtra("intentCount")) {
             String countResult = getIntent().getStringExtra("intentCount");
             report.setText(countResult);
         } else {
             report.setText("Error, could not find count!");
         }
+    }
 
-
+    void replayListener() {
+        replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LandingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

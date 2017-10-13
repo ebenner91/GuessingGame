2b56a576f5b2996b5eb02a735ca8197e90113f11
ebenner91 <ebenner@mail.greenriver.edu>
2017-10-12 18:49:23 -0700

    Added clicklistener and intent to landing activity

--- app/src/main/java/edu/greenriver/it/guessinggame/LandingActivity.java
+++ app/src/main/java/edu/greenriver/it/guessinggame/LandingActivity.java
@@ -1,13 +1,34 @@
 package edu.greenriver.it.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        getButton();
+    }
+
+    void getButton() {
+        start = (Button) findViewById(R.id.startButton);
+    }
+
+    void bindFunctionality() {
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GuessingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
